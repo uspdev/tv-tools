fa53e3e5d0431f947400a40c36da3bc8f45baf1a
Masaki Kawabata Neto <kawabata@usp.br>
2019-04-24 11:46:47 -0300

    removendo mensagem do chromium

--- kiosk.sh
+++ kiosk.sh
@@ -2,9 +2,14 @@
 
 export DISPLAY=:0
 
+# para que serve ??
 unclutter &
 
+# vamos prevenir a mensagem de restauração de páginas
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-
+# vamos chamar o chromium
+# --incognito se quiser chamar em modo anonimo: desativa cache e outras coisas
 /usr/bin/chromium-browser --kiosk /home/tvpc/tv-tools/index.html
 
