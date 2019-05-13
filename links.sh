c09862b68090b80dc91c3097da0e814abf8d3eb9
Henrique1792 <freitash1792@gmail.com>
2019-05-13 14:49:00 +0000

    arquivo de script funciona - atualização do README será feito na master

--- links.sh
+++ links.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/bash
 
-SOURCE=$HOME/tv-tools/
-AUTO=$HOME/tv-tools/.config/autostart
+SOURCE=$HOME/tv-tools
+AUTO=$HOME/.config/autostart
 
-ln -s -T $SOURCE/chromium_autostart.desktop $AUTO/chromium_autostart.desktop
-ln -s -T $SOURCE/lxrandr-autostart.desktop $AUTO/lxrandr-autostart.desktop
-ln -s -T $SOURCE:/tv-tools/vino-server.desktop $AUTO/vino-server.desktop
+ln -s $SOURCE/chromium_autostart.desktop $AUTO/chromium_autostart.desktop
+ln -s $SOURCE/lxrandr-autostart.desktop $AUTO/lxrandr-autostart.desktop
+ln -s $SOURCE/tv-tools/vino-server.desktop $AUTO/vino-server.desktop
 
-mv *.desktop $HOME/.config/autostart
 
-ln -s -T $SOURCE/index.html $HOME/index.html
-ln -s -T $SOURCE/tv-tools/kiosk.sh $HOME/kiosk.sh
+ln -s ./index.html $HOME/index.html
+ln -s ./tv-tools/kiosk.sh $HOME/kiosk.sh
 
