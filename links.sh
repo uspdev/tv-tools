0f8df59f790001cc12338694f5a8515e412169c8
Henrique1792 <freitash1792@gmail.com>
2019-05-20 13:40:55 +0000

    modificações em links para o ajuste do lightdm

--- links.sh
+++ links.sh
@@ -3,6 +3,21 @@
 SOURCE=$HOME/tv-tools
 AUTO=$HOME/.config/autostart
 
+
+#test if  /etc/lightdm/lightdm.conf exists.
+
+
+[ ! -e /etc/lightdm.conf ] && cp ./lightdm.conf /etc/lightdm.conf;
+[ ! -e /etc/lightdm.conf.d ] && mkdir /etc/lightdm.conf.d && cp ./50-myconfig.conf /etc/lightdm.conf.d;
+read -p "Informe o nome de usuário, por favor (enter para usuário padrão)" username
+
+
+if([ -z "$username" ])
+then
+	sed -i "s/tvpc/$username/g" /etc/lightdm.conf.d/lightdm.conf.d/50-myconfig.conf;
+	sed -i "s/tvpc/$username/g" /etc/lightdm.conf
+fi
+
 ln -s $SOURCE/chromium_autostart.desktop $AUTO/chromium_autostart.desktop
 ln -s $SOURCE/lxrandr-autostart.desktop $AUTO/lxrandr-autostart.desktop
 ln -s $SOURCE/tv-tools/vino-server.desktop $AUTO/vino-server.desktop
