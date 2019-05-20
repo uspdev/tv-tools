030241c72c8f166a82d14948b39fb9f3a966e804
Henrique1792 <freitash1792@gmail.com>
2019-05-20 13:51:10 +0000

    correções de path

--- links.sh
+++ links.sh
@@ -7,15 +7,15 @@ AUTO=$HOME/.config/autostart
 #test if  /etc/lightdm/lightdm.conf exists.
 
 
-[ ! -e /etc/lightdm.conf ] && cp ./lightdm.conf /etc/lightdm.conf;
-[ ! -e /etc/lightdm.conf.d ] && mkdir /etc/lightdm.conf.d && cp ./50-myconfig.conf /etc/lightdm.conf.d;
+[ ! -e /etc/lightdm.conf ] && cp ./lightdm.conf /etc/lightdm/lightdm.conf;
+[ ! -e /etc/lightdm.conf.d ] && mkdir /etc/lightdm/lightdm.conf.d && cp ./50-myconfig.conf /etc/lightdm/lightdm.conf.d;
 read -p "Informe o nome de usuário, por favor (enter para usuário padrão)" username
 
 
 if([ -z "$username" ])
 then
-	sed -i "s/tvpc/$username/g" /etc/lightdm.conf.d/lightdm.conf.d/50-myconfig.conf;
-	sed -i "s/tvpc/$username/g" /etc/lightdm.conf
+	sed -i "s/tvpc/$username/g" /etc/lightdm/lightdm.conf.d/50-myconfig.conf;
+	sed -i "s/tvpc/$username/g" /etc/lightdm/lightdm.conf
 fi
 
 ln -s $SOURCE/chromium_autostart.desktop $AUTO/chromium_autostart.desktop
