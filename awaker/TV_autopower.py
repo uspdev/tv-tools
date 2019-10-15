7ebca6ee138a29422e79ece9241f483bfa5c2a10
André Girol <andregirol@gmail.com>
2019-10-15 20:28:57 -0300

    Refatora dias específicos que PCTV fica desligadao
    Adicionados testes

--- awaker/TV_autopower.py
+++ awaker/TV_autopower.py
@@ -51,17 +51,21 @@ def dia_util(data_atual):
     return data_atual.weekday() < 4
 
 
+def fica_desligado(data_atual, dias_desligado):
+    hoje = (data_atual.day, data_atual.month)
+    return hoje in dias_desligado
+
+
 def dispara_acpi_wake():
     for ip, mac in PCTVS.items():
         # segunda a sexta      ou está no exclude               ou  é noite
-        if (! dia_util(now) or ((now.day, now.month) in NO_POWER_ON) or (now.hour >= 19) or (now.hour <= 7):
+        if not dia_util(now) or fica_desligado(now, NO_POWER_ON) or (now.hour >= 19) or (now.hour <= 7):
             os.system("echo 'ACPITOOL' >> /root/tv_log")
             os.system("ssh {} acpitool -s >> /root/tv_log".format(ip))
         else:
             os.system("echo 'Wake On Lan' >> /root/tv_log")
             os.system("etherwake -i eth0 {} >> /root/tv_log".format(mac))
             os.system("wakeonlan {} >> /root/tv_log".format(mac))
-
 
     os.system("date >> /root/tv_log")
 
--- awaker/tests.py
+++ awaker/tests.py
@@ -2,7 +2,7 @@ import unittest
 import datetime
 
 
-from TV_autopower import dia_util
+from TV_autopower import dia_util, fica_desligado
 
 
 class TestAutopower(unittest.TestCase):
@@ -22,6 +22,20 @@ class TestAutopower(unittest.TestCase):
 
         self.assertFalse(dia_util(sabado))
         self.assertFalse(dia_util(domingo))
+
+    def test_fica_desligado(self):
+        NO_POWER_ON = [
+            (1, 2),
+            (3, 4),
+            (5, 6),
+        ]
+        um_fevereiro = datetime.date(2019, 2, 1)
+        tres_abril = datetime.date(2019, 4, 3)
+        oito_julho = datetime.date(2019, 7, 8)
+
+        self.assertTrue(fica_desligado(um_fevereiro, NO_POWER_ON))
+        self.assertTrue(fica_desligado(tres_abril, NO_POWER_ON))
+        self.assertFalse(fica_desligado(oito_julho, NO_POWER_ON))
 
 
 
